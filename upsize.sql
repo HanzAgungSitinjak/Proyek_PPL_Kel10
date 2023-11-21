5714f9ffedeec19564cc010ee7bc91e03e9a5a74
HanzAgungSitinjak <hansagung19@gmail.com>
2023-11-21 10:49:31 +0700

    Code Web Baru

---
+++ upsize.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 21, 2023 at 02:50 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `upsize`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+-- Error reading structure for table upsize.bookings: #1932 - Table &#039;upsize.bookings&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.bookings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`bookings`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carts`
+--
+-- Error reading structure for table upsize.carts: #1932 - Table &#039;upsize.carts&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.carts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`carts`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kritiksaran`
+--
+-- Error reading structure for table upsize.kritiksaran: #1932 - Table &#039;upsize.kritiksaran&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.kritiksaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`kritiksaran`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mejas`
+--
+-- Error reading structure for table upsize.mejas: #1932 - Table &#039;upsize.mejas&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.mejas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`mejas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table upsize.migrations: #1932 - Table &#039;upsize.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table upsize.orders: #1932 - Table &#039;upsize.orders&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`orders`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+-- Error reading structure for table upsize.order_details: #1932 - Table &#039;upsize.order_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.order_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`order_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table upsize.personal_access_tokens: #1932 - Table &#039;upsize.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table upsize.products: #1932 - Table &#039;upsize.products&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`products`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table upsize.users: #1932 - Table &#039;upsize.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table upsize.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `upsize`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
